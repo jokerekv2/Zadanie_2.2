cf1fe788f5beda11f1e3dd182e63bcf17f4e3d5e
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-06 09:39:48 +0200

    poprawka poprawki rozwiązania

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ public class Main {
 
         drink.ingredient1 = new Ingredients();
         drink.ingredient1.name = "Strawberry";
-        drink.ingredient1.amt = 3;
+        drink.ingredient1.amount = 3;
         drink.ingredient2 = new Ingredients();
         drink.ingredient2.name = "Banana";
-        drink.ingredient2.amt = 1;
+        drink.ingredient2.amount = 1;
         drink.ingredient3 = new Ingredients();
         drink.ingredient3.name = "Cup of Milk";
-        drink.ingredient3.amt = 1;
+        drink.ingredient3.amount = 1;
 
-        int volume = drink.ingredient1.amt + drink.ingredient2.amt + drink.ingredient3.amt;
+        int volume = drink.ingredient1.amount + drink.ingredient2.amount + drink.ingredient3.amount;
 
         System.out.println("Name: " + drink.name + "\nAlcoholic: " + drink.alcoholic + "\nPrice: " + drink.price);
         System.out.println("Volume: " + volume  + "\n");
-        System.out.println("Ingredients:\n" + drink.ingredient1.name + ": " + drink.ingredient1.amt);
-        System.out.println(drink.ingredient2.name + ": " + drink.ingredient2.amt);
-        System.out.println(drink.ingredient3.name + ": " + drink.ingredient3.amt);
+        System.out.println("Ingredients:\n" + drink.ingredient1.name + ": " + drink.ingredient1.amount);
+        System.out.println(drink.ingredient2.name + ": " + drink.ingredient2.amount);
+        System.out.println(drink.ingredient3.name + ": " + drink.ingredient3.amount);
     }
 }
